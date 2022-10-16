a6bbda9cc3c105ad41f902e795648c6322025f9a
Your Name <senasezer@sena-MacBook-Air.local>
2022-10-16 23:26:25 +0300

    done

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function increment(n) {
+  return n + 1 ;
+}
+function decrement(n) {
+  return n -1;
+}
+function makeInt(a,b) {
+  return parseInt [('0x2328',10), ('sldkjflksjf', 10), (a,b)] ;
+}
+function preserveDecimal(n){
+  return parseFloat(n);
+
+}
